657cb7879e9e171831325d704390c62170061273
Eduardo Henrique <drdhenri@gmail.com>
2024-05-18 15:54:14 -0300

    questao 4 + pytest

---
+++ .ipynb_checkpoints/funcs-checkpoint.py
@@ -0,0 +1,16 @@
+def fact(N):
+    if isinstance(N, int):
+        raise TypeError("O argumento deve ser um inteiro")
+    if N < 0:
+        raise ValueError("O argumento deve ser um inteiro não-negativo")    
+    if (N==0):
+        return(1)
+    else:
+        return(N*fact(N-1))
+
+def expo(x,N):
+    if (N==0):
+        return(1)
+    else:
+        return(x**N/fact(N) + expo(x,N-1))
+  
